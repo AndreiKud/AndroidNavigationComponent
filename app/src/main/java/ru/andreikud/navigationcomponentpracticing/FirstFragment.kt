18ec2a3c4aef47d6818eb284ccb31cc20ebfb62a
Andrei <andreikudrya1995@gmail.com>
2021-09-13 16:35:30 +0500

    Basic navigation setup.

---
+++ app/src/main/java/ru/andreikud/navigationcomponentpracticing/FirstFragment.kt
@@ -0,0 +1,18 @@
+package ru.andreikud.navigationcomponentpracticing
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.btnClick).setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+
+}
--- app/src/main/java/ru/andreikud/navigationcomponentpracticing/MainActivity.kt
+++ app/src/main/java/ru/andreikud/navigationcomponentpracticing/MainActivity.kt
@@ -2,10 +2,29 @@ package ru.andreikud.navigationcomponentpracticing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcvNavHost) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph))
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
